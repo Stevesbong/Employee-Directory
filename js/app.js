e641636ae4f4cd19a7494906df2caf8aefcdb0df
Steve Park <ghkfkd0323@gmail.com>
2020-05-02 02:16:11 -0700

    displayed employees

--- js/app.js
+++ js/app.js
@@ -4,16 +4,29 @@ const gridBox = document.getElementById("employee-main");
 const url = 'https://randomuser.me/api/?results=12'
 
 async function fetcingData(url) {
-    const data = await fetch(url)
-    const dataJSON = await data.json();
-    return dataJSON;
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch ( error ) {
+        throw error;
+    }
 }
-const test = fetcingData('https://randomuser.me/api/?results=12').then(data => console.log(data))
-// fetch(url)
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         data.results.forEach(employee => {
-//             console.log(employee)  
-//         })
-//     })
+
+function disPlayEmployees(json) {
+    json.results.map( employee => {
+        console.log(employee)
+        const section = document.createElement("section");
+        section.classList.add('card')
+        gridBox.appendChild(section);
+        section.innerHTML = `
+            <img class="avatar" src="${employee.picture.large}" alt="${employee.name.title} ${employee.name.last} ${employee.name.first}">
+            <div class="text-wrapper">
+                <h4 class="name">${employee.name.first} ${employee.name.last}</h4>
+                <p class="email">${employee.email}</p>
+                <p class="city">${employee.location.city}</p>
+            </div>
+        `
+    })
+}
+fetcingData('https://randomuser.me/api/?results=12')
+    .then(disPlayEmployees)
