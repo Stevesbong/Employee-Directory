8c9cf5ca329a94702edd38cb90598b6ea98465fd
Steve Park <ghkfkd0323@gmail.com>
2020-05-02 16:21:54 -0700

    added comments

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-console.log('app working')
 
+console.log('app working')
+// GET GRID CONTAINER
 const gridBox = document.getElementById("employee-main");
+// GENERATE RANDOM EMPLOYEES
 const url = 'https://randomuser.me/api/?results=12'
 
+
+// FETCHING DATA FROM GENERATE RANDOM EMPLOYEES WEBSITE
+// USING ASYNC / AWAIT AND FETCH() METHODS TO GET DATA
 async function fetcingData(url) {
     try {
         const response = await fetch(url);
@@ -12,11 +17,18 @@ async function fetcingData(url) {
     }
 }
 
+
+// FUNCTION FOR DISPLAY ALL 12 EMPLOYEES TO THE WEBSITE
 function disPlayEmployees(json) {
+    // LOOP THROUGH EACH EMPLOYEE FROM JSON DATA
     json.results.map( employee => {
         console.log(employee)
+
+        // CREATE EACH SECTION FOR EACH EMPLOYEE
         const section = document.createElement("section");
         section.classList.add('card')
+
+        // ADD TO GRID CONTAINER
         gridBox.appendChild(section);
         section.innerHTML = `
             <img class="avatar" src="${employee.picture.large}" alt="${employee.name.title} ${employee.name.last} ${employee.name.first}">
@@ -28,5 +40,8 @@ function disPlayEmployees(json) {
         `
     })
 }
+
+
+// DISPLAY ALL 12 EMPLOYEES TO THE WEBSITE
 fetcingData('https://randomuser.me/api/?results=12')
     .then(disPlayEmployees)
